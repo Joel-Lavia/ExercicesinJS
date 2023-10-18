b61a2ffa98f58861ae0e2c5184f2815c4e103f48
Hol418 <joeldisidi8@gmail.com>
2023-10-18 18:36:43 +0100

    finish second exercices

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//declaration variable prenom
 let prenom = "Lata";
 console.log(prenom);
+//valeur de x après execution 
+let x = "Geeta";
+console.log(x);
+//variable flower
+let flower = "Rose";
+let abre = "érable";
+let y = 'Tic';
+y = 'Tac';
+y = 'Toe';
