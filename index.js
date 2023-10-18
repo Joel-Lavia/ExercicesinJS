448f0b8259a15f99ac0f91c2f5d7d584f238a3ac
JoelD-418 <joeldisidi8@gmail.com>
2023-10-19 00:49:32 +0100

    Correction of variable declaration and function definition.

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 //declaration variable prenom
-let prenom = "Lata";
-console.log(prenom);
+const prenom = "Lata";
 //valeur de x après execution 
-let x = "Geeta";
-console.log(x);
+// let x = "Geeta";
+// la valeur de x seras "Geeta"
 //variable flower
-let flower = "Rose";
-let abre = "érable";
-let y = 'Tic';
+const flower = "Rose";
+const abre = "érable";
+//quel seras la valeur de y après execution
+let y ='Tic';
 y = 'Tac';
-y = 'Toe';
-console.log(y);
+y = 'Toe'; // la valeur de y seras "Toe"
+
 //Définissez une fonction hello qui renvoie 'Hello world'
 function hello(){
 return "Hellow world";
 }
-console.log(hello());
 /*deux fonctions. La première fonction a doit renvoyer « Bonjour a ! » 
 et la deuxième fonction b devrait renvoyer « Bonjour b !*/
 function a(){
 return 'Bonjoursa!';
 }
 function b(){
-return'BonjoursB!';
+return'Bonjoursb!';
 }
-console.log(`${a()},${b()}`);
 // La fonction greet renvoyant la valeur 'Haydo!' avec un parametre  salutation variable 
-function grett(salutation){
-return salutation;
+function grett(){
+return "Haydo";
 }
-console.log(grett("Haydo"));
+const salutation = grett();
 function hello() {
-return 'Hi!';
-  }
-  
-  let saluté = hello();
-  console.log(saluté);
+  return 'Hi!';
+}
+//Écrivez une fonction echo qui renvoie également le paramètre passé. echo('Greta') devrait renvoyer 'Greta' et echo('CO2') devrait renvoyer 'CO2'
+let x = hello();//renvoie "Hi"
+function echo(parames) {
+  return parames; // renoie "Greeta" et "CO2"
+}
+
